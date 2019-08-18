8ac06dc39c52ed3d7ba0c0deca773bff583db1aa
mul118 <mikhail.lisovich@gmail.com>
2019-08-18 13:54:47 -0400

    Remove locks, add idendro, add force arg

--- cranvas_startup_patch.sh
+++ cranvas_startup_patch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Startup patch to 'properly' install cranvas, so documentation is available. 
+# Startup patch to 'properly' install cranvas & idendro, so documentation is available. 
 # Works around the issue of X server being unavailable during docker build process,
 # Which makes cranvas installation via R CMD 'fail' before docs are compiled.
 # Build process gets around this via the '--no-test-load' arg.
 
-R -e "remotes::install_github(args = '--no-multiarch', 'ggobi/cranvas')"
+rm -R /usr/local/lib/R/site-library/00LOCK-cranvas
+rm -R /usr/local/lib/R/site-library/00LOCK-idendro
+R -e "remotes::install_github(args = '--no-multiarch', c('ggobi/cranvas', 'tsieger/idendro'), force = TRUE)"
